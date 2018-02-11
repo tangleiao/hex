7ea680babbabae11340b2de5b3f65878f6db292e
tangleitao <tangleitao@xin.com>
2018-02-11 16:01:07 +0800

    test

--- laravel-modules/src/helpers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!function_exists('appCall')) {
-    function appCall($contractName, $funcName, $params)
-    {
-        return App::call($contractName.'@'.$funcName, $params);
-    }
-}
